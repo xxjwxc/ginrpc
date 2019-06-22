ac38cb1048783e7aaa7e4c80039042897b4f77ee
xiexiaojun <xie1xiao1jun@qq.com>
2019-06-22 22:16:54 +0800

    new

--- base/common.go
+++ base/common.go
@@ -17,17 +17,10 @@ import (
  func(*Context) //自定义的context类型
  func(*Context,req) //自定义的context类型,带request 请求参数
 */
-// type handlerFunc1 func(*gin.Context)
-// type handlerFunc2 func(*api.Context)
-// type handlerFunc3 func(*api.Context, interface{})
 
 func _fun1(*gin.Context)              {}
 func _fun2(*api.Context)              {}
 func _fun3(*api.Context, interface{}) {}
-
-// var fun1 handlerFunc1
-// var fun2 handlerFunc2
-// var fun3 handlerFunc3
 
 func GetHandlerFunc(handlerFunc interface{}) gin.HandlerFunc {
 	//gin默认方法
@@ -41,12 +34,6 @@ func GetHandlerFunc(handlerFunc interface{}) gin.HandlerFunc {
 		return func(c *gin.Context) {
 			method.Call([]reflect.Value{reflect.ValueOf(api.Newctx(c))})
 		}
-
-		// 带 struct 定义
-		// method := reflect.TypeOf(handlerFunc).Method(0)
-		// return func(c *gin.Context) {
-		// 	method.Func.Call([]reflect.Value{reflect.ValueOf(api.Newctx(c))})
-		// }
 	}
 
 	//自定义的context类型,带request 请求参数
@@ -110,49 +97,6 @@ func getCallFunc3(handlerFunc interface{}) (func(*gin.Context), error) {
 
 	}, nil
 }
-
-// func getCallFunc3(handlerFunc interface{}) (func(*gin.Context), error) {
-// 	typ := reflect.TypeOf(handlerFunc).Method(0)
-// 	mt := typ.Type
-// 	if mt.NumIn() != 2 { //参数检查
-// 		return nil, errors.New("method " + reflect.ValueOf(handlerFunc).Type().Name() + " not support!")
-// 	}
-
-// 	var ctxType, reqType reflect.Type
-// 	ctxType = mt.In(0)
-// 	reqType = mt.In(1)
-// 	if reflect.TypeOf(ctxType) != reflect.TypeOf(&gin.Context{}) &&
-// 		reflect.TypeOf(ctxType) != reflect.TypeOf(&api.Context{}) {
-// 		return nil, errors.New("method " + reflect.ValueOf(handlerFunc).Type().Name() + " first parm not support!")
-// 	}
-
-// 	reqIsValue := false //
-// 	if reqType.Kind() == reflect.Ptr {
-// 		reqIsValue = true
-// 	} else {
-// 		reqIsValue = false
-// 	}
-// 	method := reflect.TypeOf(handlerFunc).Method(0)
-// 	return func(c *gin.Context) {
-// 		var req reflect.Value
-// 		if reqIsValue {
-// 			req = reflect.New(reqType.Elem())
-// 		} else {
-// 			req = reflect.New(reqType)
-// 		}
-
-// 		if err := unmarshal(c, req.Interface()); err != nil { //返回错误信息
-// 			c.JSON(200, message.GetErrorMsg(message.ParameterInvalid, err))
-// 			return
-// 		}
-
-// 		if reqIsValue {
-// 			req = req.Elem()
-// 		}
-
-// 		method.Func.Call([]reflect.Value{reflect.ValueOf(api.Newctx(c)), req})
-// 	}, nil
-// }
 
 func unmarshal(c *gin.Context, v interface{}) error {
 	return c.ShouldBind(v)
