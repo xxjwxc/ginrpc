46f15651fc01098b11f4a953ef294314ede2a6a5
谢小军 <xxj@xiexiaojundeMacBook-Pro.local>
2019-08-05 11:28:53 +0800

    new

--- example_test.go
+++ example_test.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"net/http/httptest"
 	"testing"
+
+	"github.com/xxjwxc/ginrpc/base/api"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xxjwxc/ginrpc/base"
 )
 
 func TestMain(t *testing.T) {
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	TestFun1(c)
+	TestFun2(api.Newctx(c))
+	TestFun3(api.Newctx(c), &ReqTest{})
+	TestFun4(c, ReqTest{})
+
 	router := gin.Default()
 	router.POST("/test1", base.GetHandlerFunc(TestFun1))
 	router.POST("/test2", base.GetHandlerFunc(TestFun2))
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/gin-gonic/gin v1.4.0
+	github.com/stretchr/testify v1.3.0
 	github.com/xxjwxc/public v1.0.2
 )
