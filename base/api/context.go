2dec2a32054d7293445bd22ff13f93e04d314c6d
谢小军 <xxj@xiexiaojundeMacBook-Pro.local>
2019-09-22 22:58:57 +0800

    translate  better

--- base/api/context.go
+++ base/api/context.go
@@ -1,8 +1,6 @@
 // Package api The next version of the underlying category will support automatic parsing of a single struct.
 package api
 
-// 基础类目 下一个版本将支持 单个struct 自动解析。
-
 import (
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +15,7 @@ func NewCtx(c *gin.Context) *Context { // 新建一个自定义context
 	return &Context{c}
 }
 
-// GetVersion Get the version by req ruter
+// GetVersion Get the version by req url
 func (c *Context) GetVersion() string { // 获取版本号
 	return c.Param("version")
 }
